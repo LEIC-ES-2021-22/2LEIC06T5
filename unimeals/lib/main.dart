0704fca8621509a8f66e2294b8a0bfb9036b18bd
Anete <anetempereira@gmail.com>
2022-04-12 19:05:43 +0100

    Vertical prototype completed

--- unimeals/lib/main.dart
+++ unimeals/lib/main.dart
@@ -15,11 +15,11 @@ class Home extends StatelessWidget {
         backgroundColor: Color(0xFF8C2D19),
         centerTitle: true,
         shape: ContinuousRectangleBorder(borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(0), bottomRight: Radius.circular(260))),
+            bottomLeft: Radius.circular(60), bottomRight: Radius.circular(60))),
       ),
       body:
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             child: Text(
@@ -30,11 +30,51 @@ class Home extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            padding: EdgeInsets.all(50.0),
+            padding: EdgeInsets.fromLTRB(150.0, 30.0, 100.0, 30.0),
+          ),
+          Container(
+            child: Text(
+              'Framework desenvolvida pelo grupo 2LEIC06T5 no âmbito da unidade curricular Engenharia de Software.',
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Color(0xFF8C2D19),
+              ),
+            ),
+            padding: EdgeInsets.all(25.0),
+          ),
+          Container(
+            child: Text(
+              'De estudantes, para estudantes!',
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Color(0xFF8C2D19),
+              ),
+            ),
+            padding: EdgeInsets.all(25.0),
+          ),
+          Container(
+            child: Text(
+              'Integrantes do grupo:',
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Color(0xFF8C2D19),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            padding: EdgeInsets.all(25.0),
+          ),
+          Container(
+            child: Text(
+              'André Miguel Pacheco Morais\n\nAnete Medina Pereira\n\nMaria Carlota Gomes Ribeiro Matos Leite\n\nMaria Eduarda Pacheco Mendes Araújo\n\nMiguel Bravo de Almeida e Silva Figueiredo ' ,
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Color(0xFF8C2D19),
+              ),
+            ),
+            padding: EdgeInsets.all(25.0),
           )
         ],
       ),
     );
   }
 }
-
