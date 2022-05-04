43ab38403bea143ab67f8584bb4da26b160b8e1b
Anete <anetempereira@gmail.com>
2022-05-04 17:56:26 +0100

    Trying to separate main.dart into different views

--- unimeals/lib/view/general_design_view.dart
+++ unimeals/lib/view/general_design_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 
 abstract class GeneralDesignViewState extends State<StatefulWidget>{
@@ -13,6 +14,24 @@ abstract class GeneralDesignViewState extends State<StatefulWidget>{
     return Container();
   }
 
-  
+  // To organize the page
+  Widget getScaffold(BuildContext context, Widget body){
+    return Scaffold(
+      appBar: buildAppBar(context), //Implemented after
+      /*
+      *  drawer: NavigationDrawer(parentContext: context),
+      * body: this.refreshState(context, body),
+      */
+    );
+  }
+
+  AppBar buildAppBar(
+        title: Text('UniMeals'),
+        backgroundColor: Color(0xFF8C2D19),
+        centerTitle: true,
+        shape: ContinuousRectangleBorder(borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(60), bottomRight: Radius.circular(60))),
+      )
+
 
 }
