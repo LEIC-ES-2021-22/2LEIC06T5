1b2ca7cc7c2931374e0890782673cc4d0325e19b
Anete <anetempereira@gmail.com>
2022-05-10 20:30:43 +0100

    Changed the way that main.dart will get things

--- unimeals/lib/main.dart
+++ unimeals/lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:unimeals/view/pages/aboutpage.dart';
+
 import 'package:unimeals/view/pages/MainPage.dart';
 
+
 void main() {
-  runApp(MaterialApp(
-      home: HomePage())
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'UniMeals App',
+      home: HomePage(),
+    );
+  }
 }
