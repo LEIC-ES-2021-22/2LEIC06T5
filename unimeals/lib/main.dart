de4a9bc411723db328b973711bc7fd1bcc2b7abd
Anete <anetempereira@gmail.com>
2022-05-12 15:04:46 +0100

    Minor change so that main.dart can access the main page

--- unimeals/lib/main.dart
+++ unimeals/lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:unimeals/view/pages/aboutpage.dart';
+import 'package:unimeals/view/MainPage.dart';
 
-import 'package:unimeals/view/pages/MainPage.dart';
 
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'UniMeals App',
-      home: HomePage(),
+      home: RestaurantsListScreen(),
     );
   }
 }
