4cbe48a685905dabdb6b9f81dabbab96560197fa
Anete <anetempereira@gmail.com>
2022-05-12 14:58:35 +0100

    Changed way of getting data for restaurants

--- unimeals/lib/model/restaurants.dart
+++ unimeals/lib/model/restaurants.dart
@@ -1,43 +1,60 @@
 import 'package:flutter/material.dart';
 
-import 'dish.dart';
-
 class Restaurant {
   /*int id;*/
   String name;
-  String imgUrl;
+  String imageUrl;
   String location;
-  String rating;   // Depois passar rating para num
-  /*Map <String, List<Dish>> menu*/;
+  int rating;   // Depois passar rating para num
+  /*Map <String, List<Dish>> menu*/
 
-  Restaurant({
-    this.name,
-    this.imgUrl,
-    this.location,
-    this.rating
-  /*,
-      this.menu*/);
-}
+  Restaurant(
+    {this.name = "Grill",
+    this.imageUrl = 'https://sigarra.up.pt/sasup/en/imagens/SC-alimentacao-cantina-engenharia.jpg',
+    this.location = 'Algures Feup',
+    this.rating = 94
+    });
 
 }
 
 List<Restaurant> restaurantList = [
   Restaurant(
     name: 'Grill',
-    imgUrl: 'assets/images/grill_feup.jpeg'
+    imageUrl: 'https://noticias.up.pt/wp-content/uploads/2015/04/Grill_630x330.jpg',
     location: 'Algures Feup' ,
-    rating: '20%',
+    rating: 94,
   ),
   Restaurant(
-    title: 'Jumanji',
-    description:
-    'In Jumanji: The Next Level, the gang is back but the game has changed.',
-    year: 2019,
-    imageUrl: 'https://i.ytimg.com/vi/MJuFdpVCcsY/movieposter_en.jpg'),
+    name: 'Cafetaria - Restaurante Feup',
+    imageUrl: 'https://10619-2.s.cdn12.com/rests/original/801_76744147.jpg',
+    location: 'Algures Feup',
+    rating: 33,
+  ),
+  Restaurant(
+    name: 'Restaurante do INEGI',
+    imageUrl: 'https://img.restaurantguru.com/r692-Restaurante-INEGI-meals.jpg',
+    location: 'Algures Feup' ,
+    rating: 80,
+  ),
+  Restaurant(
+    name: 'Cantina - Almoço',
+    imageUrl: 'https://sigarra.up.pt/sasup/en/imagens/SC-alimentacao-cantina-engenharia.jpg',
+    location: 'Algures Feup' ,
+    rating: 29,
+  ),
+  Restaurant(
+    name: 'Cantina - Jantar',
+    imageUrl: 'https://sigarra.up.pt/sasup/en/imagens/SC-alimentacao-cantina-engenharia.jpg',
+    location: 'Algures Feup' ,
+    rating: 29,
+  ),
+  Restaurant(
+    name: 'Bar INESC TEC',
+    imageUrl: 'https://sigarra.up.pt/feup/pt/WEB_GESSI_DOCS.download_file?p_name=F962267673/Grill.jpg',
+    location: 'Algures Feup' ,
+    rating: 29,
+  )
 ];
-
-
-
 
 
 
@@ -72,7 +89,7 @@ List<Restaurant> restaurantList = [
     ];
   }*/
 
-  }
+
 
 
 /*Restaurant res0 = Restaurant(0,'Cantina FEUP');
