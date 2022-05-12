ad67df57d5a510912c800e39826f26f97a280e7e
Andrekt02 <andremiguelpachecomorais@gmail.com>
2022-05-13 00:35:12 +0100

    added hours to a file and tried adding tests

---
+++ unimeals/test_driver/app_test.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:gherkin/gherkin.dart';
+import 'hooks/hook_example.dart';
+import 'steps/colour_parameter.dart';
+import 'steps/given_I_pick_a_colour_step.dart';
+import 'steps/tap_button_n_times_step.dart';
+
+Future<void> main() {
+  final config = FlutterTestConfiguration()
+    ..features = [RegExp('features/*.*.feature')]
+    ..reporters = [
+      ProgressReporter(),
+      TestRunSummaryReporter(),
+      JsonReporter(path: './report.json')
+    ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
+    ..hooks = [HookExample()]
+    ..stepDefinitions = [TapButtonNTimesStep(), GivenIPickAColour()]
+    ..customStepParameterDefinitions = [ColourParameter()]
+    ..restartAppBetweenScenarios = true
+    ..targetAppPath = "test_driver/app.dart";
+  // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
+  return GherkinRunner().execute(config);
+}
---
+++ unimeals/test_driver/steps/tap_button_n_times_step.dart
@@ -0,0 +1,2 @@
+When I tap the "increment" button 10 times    // passes 3 parameters "increment", "button" & 10
+When I tap the "plus" icon 2 times       // passes 3 parameters "plus", "icon" & 2
