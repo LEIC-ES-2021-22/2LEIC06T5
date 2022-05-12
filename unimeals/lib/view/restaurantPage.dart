367e78d30688582581a8a17f664e4444e6021ba9
Anete <anetempereira@gmail.com>
2022-05-12 15:00:41 +0100

    Added first draft of restaurant page with image, rating and name. Navigation implemented

--- unimeals/lib/view/restaurantPage.dart
+++ unimeals/lib/view/restaurantPage.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:unimeals/constants/colors.dart';
+import 'package:unimeals/model/restaurants.dart';
 
-import '../../widgets/custom_app_bar.dart';
+class restaurantPage extends StatelessWidget {
+  final Restaurant restaurant;
 
-/*
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+  restaurantPage(this.restaurant);
 
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    var selected = 0;
     return Scaffold(
-      backgroundColor: mainRed,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(
-            Icons.arrow_back_ios,
-            Icons.menu,
-          )
-        ],
+      appBar: AppBar(
+        title: Text('UniMeals'),
+        backgroundColor: Color(0xFF8C2D19),
+        centerTitle: true,
+        shape: ContinuousRectangleBorder(borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(60), bottomRight: Radius.circular(60))),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                restaurant.imageUrl,
+                height: 500,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  restaurant.rating.toString(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 17.0, fontStyle: FontStyle.italic),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  restaurant.location,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(fontSize: 22.0),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
-    return Container();
   }
-}*/
-
+}
