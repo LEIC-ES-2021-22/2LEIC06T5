2cd5d8686e2e48af6d8a98227257dbeb30845bdc
Anete <anetempereira@gmail.com>
2022-05-18 23:01:04 +0100

    added pageNum field in class navbar to facilitate the navigation between pages

--- unimeals/lib/model/navbarItems.dart
+++ unimeals/lib/model/navbarItems.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class Navbar {
   String name;
   IconData icon;
+  int pageNum;   // For navigation when changing pages in
 
   Navbar(
       {this.name = "",
         this.icon = Icons.food_bank_outlined,
+        this.pageNum = 1
       });
 }
 
@@ -14,17 +16,21 @@ List<Navbar> navbarList = [
   Navbar(
     name: 'Página Principal',
     icon: Icons.food_bank_outlined,
+    pageNum: 1
   ),
   Navbar(
     name: 'Página de Filtros',
     icon: Icons.filter_alt_outlined,
+    pageNum: 2
   ),
   Navbar(
     name: 'Comparação de Menus',
     icon: Icons.restaurant_menu,
+    pageNum: 3
   ),
   Navbar(
     name: 'Sobre',
     icon: Icons.info_outline,
+    pageNum: 4
   )
 ];
