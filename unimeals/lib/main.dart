99789ced1962b89e9bc12cf795e109441a541664
carlotaleite <up202005428@edu.fe.up.pt>
2022-05-28 18:22:31 +0100

    Continued design of the menus

--- unimeals/lib/main.dart
+++ unimeals/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'UniMeals App',
       home: //MainPage(),
-      restaurantMenu(restaurant),
+      menusPage(restaurant),
       // MapSample(),
       // MainPage(),
     );
