d5a96baa1402001d3cd296b1a4b1a32119bb9016
Anete <anetempereira@gmail.com>
2022-06-04 16:06:26 +0100

    changed minor thing in services class trying to import json from web

--- unimeals/lib/view/pages/menuComparationPage.dart
+++ unimeals/lib/view/pages/menuComparationPage.dart
@@ -1,8 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:unimeals/model/restaurantJson.dart';
 import 'package:unimeals/view/widgets/navbarDrawer.dart';
 
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const cantinasUrl =
+    'https://sigarra.up.pt/feup/pt/mob_eme_geral.cantinas';
+
+void main() async {
+  var response = await http.get(Uri.parse(cantinasUrl));
+  if (response.statusCode == 200) {
+    var data = json.decode(response.body);
+    print(data);
+  } else {
+    throw Exception(  'Failed to read $cantinasUrl');
+  }
+}
+
+
+class Services{
+
+   /*Future<List<Restaurant>> getRestaurants() async{
+      var response = await http.get(Uri.parse(cantinasUrl));
+      if (response.statusCode == 200) {
+        final List<Restaurant> restaurants = restaurantFromJson(response.body)
+        var data = json.decode(response.body);
+        print(data);
+      } else {
+      throw Exception('Failed to weee read $cantinasUrl');
+      }
+  }*/
+
+
+}
+
+
 class MenuComparationPage extends StatelessWidget {
-  @override
+  /*@override
+  List<Restaurant> restaurants;
+  bool _loading;
+  void initState(){
+    super.initState();
+    _loading = true;
+
+  }*/
+
   Widget build(BuildContext context) {
     return Scaffold(
         drawer: navbarDrawer(),
