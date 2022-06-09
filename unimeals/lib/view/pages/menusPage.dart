10c4feb2b37948f97b8dcc9259add0efcee04c0f
carlotaleite <up202005428@edu.fe.up.pt>
2022-06-09 19:38:23 +0100

    Dish now has placeholders so menusPage is more optimized

--- unimeals/lib/view/pages/menusPage.dart
+++ unimeals/lib/view/pages/menusPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:unimeals/constants/colors.dart';
 import 'package:unimeals/model/filters.dart';
 import 'package:unimeals/model/restaurants.dart';
+import 'package:unimeals/model/dish.dart';
 import 'package:unimeals/view/widgets/changeViewButton.dart';
 import 'package:unimeals/view/widgets/navbarDrawer.dart';
 
