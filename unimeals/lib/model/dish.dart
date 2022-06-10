b9dfa0117d5657be508873d4297532609e1e0f4a
Andrekt02 <andremiguelpachecomorais@gmail.com>
2022-06-10 16:23:15 +0100

    added real dishes data to the menus

--- unimeals/lib/model/dish.dart
+++ unimeals/lib/model/dish.dart
@@ -13,99 +13,123 @@ class Dish {
       });
 }
 
-
+//restaurant 0
 List<Dish> dishGrill = [
   Dish(
-      name: 'Comidinha da Anete Grill',
+      name: 'Tortilha de carne de porco no forno com salada de alface	',
       type: 'Carne',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha ddo Grill',
+      name: 'Bolinhos de bacalhau no forno com feijão-frade',
       type: 'Peixe',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha ddo Grill',
-      type: 'Vegetariano',
-      day: 'segunda-feira'),
-  Dish(
-      name: 'Weeeeeee',
+      name: 'Abrótea cozida com batata cozida e brócolos',
       type: 'Dieta',
       day: 'segunda-feira'),
+  Dish(
+      name: 'Massa à lavrador com seitan',
+      type: 'Vegetariano',
+      day: 'segunda-feira'),
 ];
 
+//restaurant 1
 List<Dish> dishCafetaria = [
   Dish(
-      name: 'Comidinha da cafetaria	',
+      name: 'Coelho à caçador com batata cozida',
       type: 'Carne',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha da cafetaria',
+      name: 'Red fish assado com molho de mostarda e batata assada',
       type: 'Peixe',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha da cafetaria',
+      name: 'Costeletas grelhadas com arroz',
+      type: 'Dieta',
+      day: 'segunda-feira'),
+  Dish(
+      name: 'Rolo de soja assado com arroz',
       type: 'Vegetariano',
       day: 'segunda-feira'),
 ];
 
+//restaurant 2
 List<Dish> dishInegi = [
   Dish(
-      name: 'Comidinha do Inegi',
+      name: 'Peru corado recheado com legumes e massa cotovelos',
       type: 'Carne',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha do Inegi',
+      name: 'Terrina do mar (red fish, pescada, pimentos e cebola)',
       type: 'Peixe',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha do Inegi',
+      name: 'Raia grelhada com arroz branco e repolho	',
+      type: 'Dieta',
+      day: 'segunda-feira'),
+  Dish(
+      name: 'Grão estufado com legumes e arroz de coentros',
       type: 'Vegetariano',
       day: 'segunda-feira'),
 ];
 
 
-
+//restaurant 3
 List<Dish> dishInesctec = [
   Dish(
-      name: 'Comidinha do Inesc	',
+      name: 'Pá de porco no tacho com de cebolada e arroz de cenoura',
       type: 'Carne',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha do Inesc	',
+      name: 'Bacalhau cozido com todos (bacalhau, batata, ovo e repolho)',
       type: 'Peixe',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha do Inesc',
+      name: 'Fêveras de porco grelhadas com arroz branco e hortaliça',
+      type: 'Dieta',
+      day: 'segunda-feira'),
+  Dish(
+      name: 'Beringela e pimentos grelhados com feijão preto e arroz',
       type: 'Vegetariano',
       day: 'segunda-feira'),
 ];
 
+//restaurant 4
 List<Dish> dishCantinaAlmoco = [
   Dish(
-      name: 'Comidinha da Cantina	Almoço',
+      name: 'Carne de vaca estufada com ervilhas e arroz',
       type: 'Carne',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha da Cantina Almoço',
+      name: 'Maruca estufada com arroz de salsa',
       type: 'Peixe',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha da Cantina Almoço',
+      name: 'Filete de pangasius ao vapor com batata cozida e brócolos',
+      type: 'Dieta',
+      day: 'segunda-feira'),
+  Dish(
+      name: 'Lentilhas estufadas à primaveril (ervilha e cenoura) c arroz',
       type: 'Vegetariano',
       day: 'segunda-feira'),
 ];
 
+//restaurant 5
 List<Dish> dishCantinaJantar = [
   Dish(
-      name: 'Comidinha da Cantina	Jantar',
+      name: 'Carne de porco à portuguesa',
       type: 'Carne',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha da Cantina Jantar',
+      name: 'Pargo grelhado com arroz de salsa',
       type: 'Peixe',
       day: 'segunda-feira'),
   Dish(
-      name: 'Comidinha da Cantina Jantar',
+      name: 'Perna de frango cozida com batata cozida',
+      type: 'Dieta',
+      day: 'segunda-feira'),
+  Dish(
+      name: 'Salteado de tofu e legumes com batata assada',
       type: 'Vegetariano',
       day: 'segunda-feira'),
 ];
